0a056a19d64f59e068e35214561a36ef104e0081
umoh christian <umohchristian001@gmail.com>
2022-10-19 15:05:33 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+/**
+ * file - main.h
+ * author - Umoh christian
+ * Describtion - a header file containing all the prototype functions used in the 0x02-functions_nested_loops directory
+ */
+
+int _putchar(char c);
