76d2caadcdcc6ef31ec3058619298b6088ef0a47
umoh christian <umohchristian001@gmail.com>
2022-10-21 04:31:41 -0700

    new changes

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,8 +2,8 @@
 
 /**
   * mul - Multiplies two integers
-  * @x: The first number to multiply
-  * @y: The second number to multiply with first number
+  * @a: The first number to multiply
+  * @b: The second number to multiply with first number
   *
   * Return: The result of multiplication between x param and y param
   */
